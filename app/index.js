5bc692601cd964c2677683022c26b07dc4b14048
Josh Hodgson <josh.pbh@gmail.com>
2016-03-09 14:41:59 +0000

    Beutify

--- app/index.js
+++ app/index.js
@@ -11,34 +11,44 @@ app.set('view engine', 'ejs');
 var CasparCG = require('caspar-cg');
 
 var ccg = new CasparCG({
-	host: config.caspar.host,
-	port: config.caspar.port,
-	debug: config.caspar.debug
+  host: config.caspar.host,
+  port: config.caspar.port,
+  debug: config.caspar.debug
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
-  socket.on('play', function(payload){
-		console.log("Playing " + payload.file)
-    ccg.play("1-1", payload.file, {"loop": true});
+  socket.on('play', function(payload) {
+    console.log("Playing " + payload.file);
+    ccg.play("1-1", payload.file, {
+      "loop": true
+    });
   });
-  socket.on('stop', function(){ccg.clear("1-1");});
+  socket.on('stop', function() {
+    ccg.clear("1-1");
+  });
 });
-function getFiles(){
-	ccg.getMediaFiles(function (err, serverInfo) {
-		if(config.caspar.debug){console.log(serverInfo)}
-	files = serverInfo;
- });
+
+function getFiles() {
+  ccg.getMediaFiles(function(err, serverInfo) {
+    if (config.caspar.debug) {
+      console.log(serverInfo);
+    }
+    files = serverInfo;
+  });
 }
 
 var files = ["Not yet connected"];
-ccg.connect(function(){
-	files = ["Connected, please try again"];
+ccg.connect(function() {
+  files = ["Connected, please try again"];
   getFiles();
 });
 
 
 app.get('/', function(req, res) {
-  res.render('pages/home', {"version": version, "files": files});
+  res.render('pages/home', {
+    "version": version,
+    "files": files
+  });
   getFiles();
 });
